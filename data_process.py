841d61cae03be7b8ab22ac416e2f0adbf890e10e
bchiu42 <blchiu@wisc.edu>
2023-08-02 01:59:42 -0500

    Added Test, combining Test and Train, and homology

--- data_process.py
+++ data_process.py
@@ -1,2 +1,7 @@
-import pytorch
-import pandas as pd
+import requests
+
+# response = requests.get("https://string-db.org/api/tsv/homology?identifiers=[your_identifiers]")
+# https://string-db.org/api/tsv/homology?identifiers=CDK1%0dCDK2
+
+response = requests.get("https://string-db.org/api/tsv/homology?identifiers=A0A009IHW8")
+print(response)
